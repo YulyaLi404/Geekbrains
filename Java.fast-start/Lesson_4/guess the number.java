efbcb8fa48d17875d835eae8888dd21e69278c2e
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-27 14:04:25 +0300

    Update guess the number.java

--- Java.fast-start/Lesson_4/guess the number.java
+++ Java.fast-start/Lesson_4/guess the number.java
@@ -13,24 +13,24 @@ public class random {
 
     public static void main(String[] args) throws IOException {
 
-        int range = 1000;
-        int n = (int) (Math.random() * range);
-
-        playGo(n, range);
+        for (int i = 100; i <= 1000; i +=200) playGo(i);
+        System.out.println("WIN!!!");
         reader.close();
     }
 
-    private static void playGo(int num, int range) throws IOException {
-        System.out.println("Введите число от 0 до " + range);
+    private static void playGo(int num) throws IOException {
+        int a = (int) (Math.random() * num);
+
+        System.out.println("Введите число от 0 до " + num);
 
         while (true) {
             int n = Integer.parseInt(reader.readLine());
 
-            if (num == n) {
+            if (a == n) {
                 System.out.println("Win!");
                 break;
             } else
-            if (num < n) {
+            if (a < n) {
                 System.out.println("Меньше");
             } else {
                 System.out.println("Больше");
@@ -38,33 +38,3 @@ public class random {
         }
     }
 }
-
-/* Output:
-
-Введите число от 0 до 1000
-23
-Больше
-1000
-Меньше
-34
-Больше
-500
-Меньше
-400
-Меньше
-100
-Больше
-200
-Меньше
-150
-Больше
-160
-Меньше
-155
-Больше
-156
-Больше
-157
-Win!
-
-*/
